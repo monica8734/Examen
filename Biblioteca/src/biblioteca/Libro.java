c3eea3350d71c33ed4a7ae10d091e476bdaf1521
Mónica <monica.mallen8@gmail.com>
2021-05-10 19:50:11 +0200

    Incluimos la clase Libro

---
+++ Biblioteca/src/biblioteca/Libro.java
@@ -0,0 +1,31 @@
+package biblioteca;
+
+/** Clase para establecer los atributos de los libros.
+ * @author Mónica Mallén
+ * @version 1.0
+ */
+public class Libro{
+    
+
+    private String nombre;
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    
+}
